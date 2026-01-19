79b2f65974ae06e514e05cbac4aac8f9ee336c8c
William Carson <wcarson@bardytechnologies.com>
2026-01-18 22:11:51 -0800

    Fixed active state visualization

--- crates/bevy_gearbox_protocol/src/components.rs
+++ crates/bevy_gearbox_protocol/src/components.rs
@@ -4,19 +4,19 @@ pub const NAME: &str = "bevy_ecs::name::Name";
 
 // Phase 1: reflect type-path strings used by BRP world.* queries
 // State machine domain types in bevy_gearbox
-pub const STATE_MACHINE: &str = "bevy_gearbox::StateMachine";
-pub const STATE_CHILDREN: &str = "bevy_gearbox::Substates";
-pub const TRANSITIONS: &str = "bevy_gearbox::transitions::Transitions";
-pub const TARGET: &str = "bevy_gearbox::transitions::Target";
-pub const TARGETED_BY: &str = "bevy_gearbox::transitions::TargetedBy";
-pub const ALWAYS_EDGE: &str = "bevy_gearbox::transitions::AlwaysEdge";
-pub const DELAY: &str = "bevy_gearbox::transitions::Delay";
-pub const EDGE_KIND: &str = "bevy_gearbox::transitions::EdgeKind";
-pub const INITIAL_STATE: &str = "bevy_gearbox::InitialState";
-pub const STATE_MACHINE_ID: &str = "bevy_gearbox::StateMachineId";
+pub const STATE_MACHINE: &str = "bevy_gearbox_core::StateMachine";
+pub const STATE_CHILDREN: &str = "bevy_gearbox_core::Substates";
+pub const TRANSITIONS: &str = "bevy_gearbox_core::transitions::Transitions";
+pub const TARGET: &str = "bevy_gearbox_core::transitions::Target";
+pub const TARGETED_BY: &str = "bevy_gearbox_core::transitions::TargetedBy";
+pub const ALWAYS_EDGE: &str = "bevy_gearbox_core::transitions::AlwaysEdge";
+pub const DELAY: &str = "bevy_gearbox_core::transitions::Delay";
+pub const EDGE_KIND: &str = "bevy_gearbox_core::transitions::EdgeKind";
+pub const INITIAL_STATE: &str = "bevy_gearbox_core::InitialState";
+pub const STATE_MACHINE_ID: &str = "bevy_gearbox_core::StateMachineId";
 
 // Editor-exposed trackers (from bevy_gearbox::server)
-pub const TRANSITION_FEED: &str = "bevy_gearbox::server::TransitionFeed";
+pub const TRANSITION_FEED: &str = "bevy_gearbox_core::server::TransitionFeed";
 
 // Substring used to detect generic event edge component types like ...EventEdge<...>
 pub const EVENT_EDGE_SUBSTR: &str = "EventEdge";
